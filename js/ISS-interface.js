fce6f1e9fffe3cb665ad743436999e6b97d962fe
Guest User <Guest@Epicodus-6E.local>
2017-06-29 10:27:01 -0700

    move code to backend and connect to frontend

--- js/ISS-interface.js
+++ js/ISS-interface.js
@@ -1,34 +1,18 @@
-var apikey = 'AIzaSyApIDDb9M0b3j4dH77mcqqXj-dZ-l3YkhY';
+var Weather = require('./../js/ISS.js').weatherModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
-      var number = data['number'];
-      $('#spacepeeps').html(number);
-
-      data['people'].forEach(function (data) {
-           $('#astronames').append('<li>' + data['name'] + '</li>');
-      });
-  });
-
+  var spacePeople = new Weather();
+  spacePeople.getWeather();
 
   $('#location').submit(function(event) {
     event.preventDefault();
 
     var city = $('#city').val();
     var state = $('#state').val();
+    var amount = $('.amount').val();
 
-    $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apikey,
-    function(object) {
-      var lat = object.results[0].geometry.location.lat;
-      var lon = object.results[0].geometry.location.lng;
-      var amount = $('.amount').val();
-      $.getJSON('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&alt=10&n=' + amount + '&callback=?',
-      function(data) {
-        data['response'].forEach(function (data) {
-          var date = new Date(data.risetime * 1000);
-          $('#isspass').append('<li>' + date.toString() + '</li>');
-        });
-      });
-    });
-  });
-});
+    var spacePlaces = new Weather();
+    spacePlaces.getLocation(city, state, amount, apiKey);
+  }); //close #location submit
+}); //close document ready
---
+++ js/ISS.js
@@ -0,0 +1,33 @@
+function Weather() {
+
+}
+
+Weather.prototype.getWeather = function() {
+  $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+      var number = data['number'];
+      $('#spacepeeps').html(number);
+
+      data['people'].forEach(function (data) {
+           $('#astronames').append('<li>' + data['name'] + '</li>');
+      });
+  });
+};
+
+Weather.prototype.getLocation = function(city, state, amount, apiKey) {
+  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apiKey).then(function(object) {
+    var lat = object.results[0].geometry.location.lat;
+    var lon = object.results[0].geometry.location.lng;
+    console.log(lat, lon);
+    console.log(city, state, amount);
+    $('#isspass').empty();
+    $.getJSON('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&alt=10&n=' + amount + '&callback=?').then(function(data) {
+      console.log(data);
+      data['response'].forEach(function (i) {
+        var date = new Date(i.risetime * 1000);
+        $('#isspass').append('<li>' + date.toString() + '</li>');
+      }); //close ForEACh
+    }); //close then
+  }); //close get
+}
+
+exports.weatherModule = Weather;
