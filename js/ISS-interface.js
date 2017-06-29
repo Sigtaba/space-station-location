cc21ec8c7fcf77c5b8ba6fe6a96fa10a62df8803
Isabella Abatgis and Maria Del Castillo <isabella+maria@>
2017-06-29 14:53:17 -0700

    refactor code to separate front end and back end

--- js/ISS-interface.js
+++ js/ISS-interface.js
@@ -1,18 +1,35 @@
 var SpaceStation = require('./../js/ISS.js').weatherModule;
-var apiKey = require('./../.env').apiKey;
+// var apiKey = require('./../.env').apiKey;
+
+var displayDates = function(data) {
+  data['response'].forEach(function (i) {
+    var date = new Date(i.risetime * 1000);
+    $('#isspass').append('<li>' + date.toString() + '</li>');
+  }); //close ForEACh
+}
+
+var displayPeopleInSpace = function(data) {
+  var number = data['number'];
+  $('#spacepeeps').html(number);
+
+  data['people'].forEach(function (data) {
+       $('#astronames').append('<li>' + data['name'] + '</li>');
+  });
+}
 
 $(document).ready(function() {
   var spacePeople = new SpaceStation();
-  spacePeople.getAstronauts();
+  spacePeople.getAstronauts(displayPeopleInSpace);
 
   $('#location').submit(function(event) {
     event.preventDefault();
+    $('#isspass').empty();
 
     var city = $('#city').val();
     var state = $('#state').val();
     var amount = $('.amount').val();
 
     var spacePlaces = new SpaceStation();
-    spacePlaces.getLocation(city, state, amount, apiKey);
+    spacePlaces.getLocation(city, state, amount, displayDates);
   }); //close #location submit
 }); //close document ready
--- js/ISS.js
+++ js/ISS.js
@@ -1,32 +1,34 @@
-function SpaceStation() {
+var apiKey = require('./../.env').apiKey;
 
+function SpaceStation() {
+  this.apiKey = apiKey;
 }
 
-SpaceStation.prototype.getAstronauts = function() {
+SpaceStation.prototype.getAstronauts = function(displayPeopleInSpace) {
   $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
-      var number = data['number'];
-      $('#spacepeeps').html(number);
-
-      data['people'].forEach(function (data) {
-           $('#astronames').append('<li>' + data['name'] + '</li>');
-      });
+    displayPeopleInSpace(data);
   });
 };
 
-SpaceStation.prototype.getLocation = function(city, state, amount, apiKey) {
-  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apiKey).then(function(object) {
+SpaceStation.prototype.getLocation = function(city, state, amount, displayDates) {
+  var that = this;
+  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + that.apiKey).then(function(object) {
     var lat = object.results[0].geometry.location.lat;
     var lon = object.results[0].geometry.location.lng;
     console.log(city, state, amount);
-    $('#isspass').empty();
-    $.getJSON('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&alt=10&n=' + amount + '&callback=?').then(function(data) {
-      console.log(data);
-      data['response'].forEach(function (i) {
-        var date = new Date(i.risetime * 1000);
-        $('#isspass').append('<li>' + date.toString() + '</li>');
-      }); //close ForEACh
-    }); //close then
+    that.getDates(lat, lon, amount, displayDates);
   }); //close get
+}
+
+SpaceStation.prototype.getDates = function(lat, lon, amount, displayDates) {
+  $.getJSON('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&alt=10&n=' + amount + '&callback=?').then(function(data) {
+    console.log(data);
+    displayDates(data);
+    // data['response'].forEach(function (i) {
+    //   var date = new Date(i.risetime * 1000);
+    //   $('#isspass').append('<li>' + date.toString() + '</li>');
+    // }); //close ForEACh
+  }); //close then
 }
 
 exports.weatherModule = SpaceStation;
