c3eb880d280b4f0fa42080c4aacca0e6a5cbdedd
Isabella Abatgis and Maria Del Castillo <isabella+maria@>
2017-06-29 16:48:57 -0700

    remove code

--- js/ISS.js
+++ js/ISS.js
@@ -24,10 +24,6 @@ SpaceStation.prototype.getDates = function(lat, lon, amount, displayDates) {
   $.getJSON('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&alt=10&n=' + amount + '&callback=?').then(function(data) {
     console.log(data);
     displayDates(data);
-    // data['response'].forEach(function (i) {
-    //   var date = new Date(i.risetime * 1000);
-    //   $('#isspass').append('<li>' + date.toString() + '</li>');
-    // }); //close ForEACh
   }); //close then
 }
 
